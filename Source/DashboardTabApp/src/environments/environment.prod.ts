ad7f9dbb5f3f56c604c20817bb741cd1a638d0c5
tianyugo <38149678+tianyugo@users.noreply.github.com>
2022-05-28 10:45:11 +0800

    update

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://guasunqbot.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "ea41b0cb-48e1-40f7-bdf5-711f5e071724",
+        clientId: "fd303888-96b7-49f4-9139-c6b9db6ac7bf",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
